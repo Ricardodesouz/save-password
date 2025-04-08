bb225dddd86181435566a004a2243d6416820ed5
Ricardodesouz <ricardodesouza360@gmail.com>
2025-04-08 14:26:00 -0300

    Created project

---
+++ src/main/java/com/turtle/savepassword/SavePasswordApplication.java
@@ -0,0 +1,13 @@
+package com.turtle.savepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavePasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavePasswordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turtle/savepassword/SavePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.savepassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavePasswordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
